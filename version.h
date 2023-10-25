d27d90066dc853e72cf8072bc587001f2aadf7b2
Sean McGovern <gseanmcg@gmail.com>
2023-10-25 16:53:05 -0400

    configure: support out-of-tree builds

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-#define VER_NUMBER 20230628-en
+#define VER_NUMBER 20240111-en
 #define ONS_VERSION xstr(VER_NUMBER)
 #define ONS_CODENAME xstr(Turducken)
 #define NSC_VERSION 294
