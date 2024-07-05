d8cf1248f756ee89f6ad18d9d06c56c97425d3c1
Sean McGovern <gseanmcg@gmail.com>
2024-07-05 00:48:27 -0400

    [WIP] effects: add a framework for DSP versions

---
+++ ONScripterLabel_effect.h
@@ -0,0 +1,19 @@
+/* -*- C++ -*-
+ *
+ *  ONScripterLabel_effect.h
+ */
+
+#ifndef __ONSCRIPTER_LABEL_EFFECT_H__
+#define __ONSCRIPTER_LABEL_EFFECT_H__
+
+namespace ons_effect {
+}
+
+namespace ons_math {
+	typedef struct math_funcs {
+	    void (*sin)(double);
+	    void (*cos)(double);
+	} math_funcs;
+}
+
+#endif
